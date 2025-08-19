889c7e4a672327f020b5d3c4a94e1f92f7514b73
Dduk-Dduk <121329652+Dduk-Dduk@users.noreply.github.com>
2025-08-18 20:31:08 -0700

    Create operator function
    Calls math functions depending on operator input

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ function divide(a,b){
 let first
 let second
 let operator
+
+function operation(first, second, operator){
+    if (operator == "+"){
+        add(first,second)
+    } else if (operator == "-"){
+        sub(first,second)
+    } else if (operator == "*"){
+        mult(first,second)
+    } else if (operator == "/"){
+        divide(first,second)
+    } else {
+        return "Invalid operator!"
+    }
+        
+}
