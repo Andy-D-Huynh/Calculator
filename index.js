2cd51453b84842366425865d2e3de4a344941a95
Dduk-Dduk <121329652+Dduk-Dduk@users.noreply.github.com>
2025-08-19 13:50:53 -0700

    Add event listeners to operator buttons
    Shows operators in display after clicking

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ function operation(first, second, operator){
 
 const container = document.querySelector(".container")
 const display = document.querySelector(".display")
+const operators = document.querySelectorAll(".operators")
 let value = parseInt(display.value)
 
 
 for(let i = 0; i < 10; i++){
     let button = document.createElement("button")
     button.textContent = `${i}`
-    button.addEventListener("click", () =>
-    {
+    button.addEventListener("click", () => {
         display.value += `${button.textContent}`
         value = parseInt(display.value)
         console.log(value)
@@ -49,3 +49,14 @@ for(let i = 0; i < 10; i++){
     container.appendChild(button)
 }
 
+let operatorsArr = [...operators]
+
+operatorsArr.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        display.value += `${operator.textContent}`
+    })
+})
+
+// operators.addEventListener("click", () => {
+//     display.value += `${operators.textContent}`      
+// })
